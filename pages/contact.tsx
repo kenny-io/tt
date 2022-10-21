1fbc4b2599dde53d933536d59d864f69cefdc7d5
kenny-io <ekeneeze3@gmail.com>
2022-10-21 18:22:00 +0100

    add contact page

---
+++ pages/contact.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+export default function Contact() {
+  return (
+    <>
+      <Head>
+        <title>Contact Us</title>
+        <meta
+          property="og:image"
+          content="https://og-test-chi.vercel.app/api/dynamic-title?title=Contact Our Company"
+        />
+      </Head>
+      <div>
+        <h1>About Us</h1>
+      </div>
+    </>
+  );
+}
